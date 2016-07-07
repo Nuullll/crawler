0ed7943266a9a74753de448ede743073f3c4f990
Nuullll <vfirst218@gmail.com>
2016-07-07 11:22:15 +0800

    Initial commit.

---
+++ src/main/java/com/example/crawler/Crawler.java
@@ -0,0 +1,11 @@
+package com.example.crawler;
+        
+/**
+ * Created by Nuullll on 2016/7/7.
+ *
+ * Practice for grabbing data from internet.
+ * To get news titles @ http://news.ifeng.com/listpage/11502/0/1/rtlist.shtml
+ */
+public class Crawler {
+    
+}
